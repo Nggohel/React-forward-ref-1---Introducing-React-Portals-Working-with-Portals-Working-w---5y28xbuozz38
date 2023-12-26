ccfcd11ac07cef007c1df37173c97acc50908c0e
Nggohel <gohelnidhi15898@gmail.com>
2023-12-26 11:59:24 +0530

    add logic

--- app/InputField.js
+++ app/InputField.js
@@ -1,11 +1,17 @@
-'use client'
-import React ,{useRef,useState,useEffect} from 'react';
-const InputField=()=>{
-    const ref = useRef()
-    return(
+import React, { useRef } from 'react';
+
+const InputField = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const getInputValue = () => {
+    return inputRef.current.value;
+  };
+
+  return (
     <div>
-      <input id="input" type="text"  ref={ref}/>
+      <input id="input" type="text" ref={inputRef} />
     </div>
-    )
-    }
+  );
+});
+
 export default InputField;
--- app/page.js
+++ app/page.js
@@ -1,31 +1,34 @@
-'use client'
-import React,{useRef, useState} from 'react'
-import InputField from './InputField'
+import React, { useRef, useState } from "react";
+import InputField from "./InputField";
 
 function Home() {
-
-  const newRef=useRef();
-  const [values,setValues] = useState("")
+  const newRef = useRef();
+  const [values, setValues] = useState("");
 
   const settingValue = () => {
-
-  }
+    const inputValue = newRef.current.getInputValue();
+    setValues(inputValue);
+  };
 
   const focusInput = () => {
-
-  }
-
+    newRef.current.focus();
+  };
 
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/>
-    <br/>
-    <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-    <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
-
+      <InputField ref={newRef} />
+      <br />
+      <button id="settingValueButton" onClick={settingValue}>
+        Set Value
+      </button>
+      <button id="focusInputButton" onClick={focusInput}>
+        Focus the input
+      </button>
+      <br />
+      <br />
+      <textarea id="textarea" value={values}></textarea>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
